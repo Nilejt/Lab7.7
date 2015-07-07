fe0d7fa200173bbe1c9e2de51cd7d29d46d3ca2e
Nile Tate <Nilejt@gmail.com>
2015-07-07 14:24:43 -0400

    Lab 7.7

---
+++ Lab7.7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = args.Length;
+
+            for (int i = size - 1; i >= 0; i--) 
+            {
+                string word = args[i];
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < size; i++)
+            { 
+                string word = args[i].ToUpper();
+                Console.Write(word + ",", " ");
+            }
+
+
+        }
+    }
+}
